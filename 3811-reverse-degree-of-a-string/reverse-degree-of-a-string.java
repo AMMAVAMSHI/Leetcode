dd4752198bc176d186960ec053441caee48549f9
AMMA VAMSHI <ammavamshi16@gmail.com>
2026-09-20 11:04:42 +0530

    Time: 1 ms (100.00%) | Memory: 44.2 MB (34.29%) - LeetSync

--- 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.java
+++ 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.java
@@ -1,16 +1,11 @@
 class Solution {
     public int reverseDegree(String s) {
-        Map<Character,Integer> map = new HashMap<>();
-        int num = 27;
-        for(char i = 'a';i<='z';i++){
-            num = num-1;
-            map.put(i,num);
-        }
-        //System.out.println(map);
         int degree = 0;
 
         for(int i=0;i<s.length();i++){
-            degree += map.get(s.charAt(i))*(i+1);
+            char ch = s.charAt(i);
+            int num = ('z'-ch+1)*(i+1);
+            degree += num;
         }
         return degree;
     }
